c35f8756243e2abae18e202478afcee4a8607499
Amadeus <amadeusaleksei@gmail.com>
2022-08-03 17:04:55 -0400

    Added styling to Description section.

--- src/components/Hotelview/Hotelview.js
+++ src/components/Hotelview/Hotelview.js
@@ -52,6 +52,42 @@ const Hotelview = () => {
                     <div className="overview__rating-count">420 votes</div>
                 </div>
             </div>
+
+            <div className="detail">
+                <div className="description">
+                    <p className="paragraph">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi nisi dignissimos debitis ratione sapiente saepe. Accusantium cumque, quas, ut corporis incidunt deserunt quae architecto voluptate.
+                    </p>
+                    <p className="paragraph">
+                        Accusantium cumque, quas, ut corporis incidunt deserunt quae architecto voluptate delectus, inventore iure aliquid aliquam.
+                    </p>
+                    <ul className="list">
+                        <li className="list__item">Close to the beach</li>
+                        <li className="list__item">Breakfast included</li>
+                        <li className="list__item">Free airport shuttle</li>
+                        <li className="list__item">WiFi in all rooms</li>
+                        <li className="list__item">AC and heating available</li>
+                        <li className="list__item">Pets allowed</li>
+                        <li className="list__item">All languages spoken</li>
+                        <li className="list__item">Perfect for families</li>
+                    </ul>
+                    <div className="recommend">
+                        <p className="recommend__count">
+                            Marcus and 2 other friends recommend this hotel.
+                        </p>
+                        <div className="recommend__friends">
+                            <img src={require("../../img/user-1.jpg")} alt="Friend 1" className="recommend__photo"/>
+                            <img src={require("../../img/user-2.jpg")} alt="Friend 2" className="recommend__photo"/>
+                            <img src={require("../../img/user-3.jpg")} alt="Friend 3" className="recommend__photo"/>
+                            <img src={require("../../img/user-4.jpg")} alt="Friend 4" className="recommend__photo"/>
+                        </div>
+                    </div>
+                </div> 
+
+                <div className="user-review">
+                    user reviews
+                </div>
+            </div>
         </main> 
     )
 }

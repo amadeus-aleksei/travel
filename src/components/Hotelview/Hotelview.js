43c66c7c438aa571b64d52a92f1924eb34a17dec
Amadeus <amadeusaleksei@gmail.com>
2022-08-04 01:48:58 -0400

    Finished main SCSS styling except media queries.

--- src/components/Hotelview/Hotelview.js
+++ src/components/Hotelview/Hotelview.js
@@ -84,9 +84,47 @@ const Hotelview = () => {
                     </div>
                 </div> 
 
-                <div className="user-review">
-                    user reviews
-                </div>
+                <div className="user-reviews">
+                    <figure className="review">
+                        <blockquote className="review__text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga doloremque architecto dicta animi, totam, itaque officia ex.
+                        </blockquote>
+                        <figcaption className="review__user">
+                            <img src={require("../../img/user-3.jpg")} alt="User 1" className="review__photo"/>
+                            <div className="review__user-box">
+                                <p className="review__username">Nick Smith</p>
+                                <p className="review__user-date">23 Feb 2022</p>
+                            </div>
+                            <div className="review__rating">7.8</div>
+                        </figcaption>
+                    </figure>
+
+                    <figure className="review">
+                        <blockquote className="review__text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga doloremque architecto dicta animi.
+                        </blockquote>
+                        <figcaption className="review__user">
+                            <img src={require("../../img/user-2.jpg")} alt="User 1" className="review__photo"/>
+                            <div className="review__user-box">
+                                <p className="review__username">Mary Thomas</p>
+                                <p className="review__user-date">28 Jul 2022</p>
+                            </div>
+                            <div className="review__rating">9.3</div>
+                        </figcaption>
+                    </figure>
+
+                    <button className="btn-inline">Show all<span>&rarr;</span></button>
+                </div>                
+            </div>
+
+            <div className="cta">
+                <h2 className="cta__book-now">
+                    Good news! We have 4 discounted rooms for your selected dates!
+                </h2>
+                <button className="btn">
+                    <span className="btn__visible">Book now</span>
+                    <span className="btn__invisible">Only 4 rooms left</span>
+                </button>
             </div>
         </main> 
     )
